e371c5484027caf756992c4a07881572e7d2aadd
James Larisch <5074906+semaj@users.noreply.github.com>
2020-10-22 14:30:05 -0400

    Add shell.nix for me (James)

---
+++ shell.nix
@@ -0,0 +1,10 @@
+# This imports the nix package collection,
+# so we can access the `pkgs` and `stdenv` variables
+let
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+in
+{ nixpkgs ? import <nixpkgs> {} }:
+with nixpkgs; mkShell {
+  #buildInputs = [ hello unstable.elmPackages.elm ];
+  buildInputs = with pkgs; [ cargo rustc carnix autoconf libtool texinfo automake115x ];
+}
