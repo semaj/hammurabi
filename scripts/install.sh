f52f426d340e255f837a6e59cd665a866572dc8c
James Larisch <5074906+semaj@users.noreply.github.com>
2020-10-27 12:32:25 -0400

    Silent apt install

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@ set -v
 set -e
 
 sudo apt update
-sudo apt install automake-1.15 libtool autoconf texinfo
+sudo apt install -yy automake-1.15 libtool autoconf texinfo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 git submodule init
 git submodule update
