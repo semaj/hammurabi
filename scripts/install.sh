c519b217fc4e67648df6f13b521c8e73d16de2f4
James Larisch <5074906+semaj@users.noreply.github.com>
2020-10-27 12:30:41 -0400

    Add install script for cloud testing

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+set -v
+set -e
+
+sudo apt update
+sudo apt install automake-1.15 libtool autoconf texinfo
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+git submodule init
+git submodule update
+cd lib/datalog
+./configure
+make
+cd ../..
