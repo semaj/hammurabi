bf11c75b34167065a30747ec856f515e036921b8
James Larisch <5074906+semaj@users.noreply.github.com>
2020-11-09 17:12:15 -0500

    Forward OCSP flag from browser shell scripts to clients

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # chrome.sh <path to chain file> <hostname to validate against>
-RUST_BACKTRACE=1 SCRIPT=chrome ./target/debug/single $1 $2
+RUST_BACKTRACE=1 SCRIPT=chrome ./target/debug/single $1 $2 $3
