ed7108bfd4602e8f549699478e21e24c1d59e79c
Yujie Wang <71992156+esxcfrd@users.noreply.github.com>
2021-06-23 13:49:46 -0400

    Update zlintV3.pl

--- datalog/static/zlintV3.pl
+++ datalog/static/zlintV3.pl
@@ -1,6 +1,7 @@
 :- use_module(certs).
 :- use_module(std).
 :- use_module(env).
+:- use_module(ext).
 
 % sub_ca_cert: ca field MUST be set to true.
 caIsCa(Cert) :-
@@ -11,8 +12,14 @@ basicConstaintsMustBeCritical(Cert) :-
   certs:basicConstraintsExt(Cert, true),
   certs:basicConstraintsCritical(Cert, true).
   
-% sub_ca_cert: crl DistPoint MUST be present & NOT marked critical.
+subCertGivenOrSurnameHasCorrectPolicy(Cert) :- 
+  certs:givenName(Cert, Given),
+  \+ext:equal(Given, ""),
+  certs:certificatePolicies(Cert, Oid),
+  ext:equal(Oid, "2.23.140.1.2.3").
 
-%   certs:fingerprint(Cert, Fingerprint),
-%   crl_set(Fingerprint).
-  
+subCertGivenOrSurnameHasCorrectPolicy(Cert) :-
+  certs:surname(Cert, Surname),
+  \+ext:equal(Surname, ""),
+  certs:certificatePolicies(Cert, Oid),
+  ext:equal(Oid, "2.23.140.1.2.3").
