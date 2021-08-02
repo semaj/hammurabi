1ca372767c3d2ea9028e0e135b4bb464bb295be5
ktorsh <ktorsh@terpmail.umd.edu>
2021-08-02 19:00:43 +0000

    Add in a prolog driver in pyswip

---
+++ datalog/static/driver.py
@@ -0,0 +1,27 @@
+from pyswip import Prolog 
+import sys
+prolog = Prolog()
+prolog.consult(sys.argv[1])
+
+def isEmpty(s): 
+    for i in s: 
+        return False 
+    return True
+
+if (len(argv) > 3): 
+    app_query = prolog.query(sys.argv[3] + "(X)")
+    applies = not isEmpty(app_query)
+    if not applies: 
+        print("NA")
+        
+if applies: 
+    soln = prolog.query(sys.argv[2] + "(X)")
+    if not isEmpty(soln): 
+        print("Pass")
+    else: 
+        print("Fail")
+
+def isEmpty(s): 
+    for i in s: 
+        return False 
+    return True
